f7f7d7381c0c8766f719539e0c0a6d8139eecfaa
lunale13 <89617116+lunale13@users.noreply.github.com>
2021-08-26 21:42:06 -0500

    Update script.js

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+var campo_texto = document.getElementById("input");
+var boton = document.getElementById("button");
+var alerta = document.getElementById("alert");
+var lista = document.getElementById("list");
+
+
+
+
+lista.addEventListener("click", lista);
